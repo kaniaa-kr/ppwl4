e100479c35a28c780bb7f6fa327ddf2753ed9f0b
kaniaa-kr <kaniaa.kirani@gmail.com>
2026-03-02 14:13:44 +0700

    PPWL 4 - Praktikum 1

---
+++ src/middleware.ts
@@ -0,0 +1,120 @@
+import { Elysia, t } from "elysia"
+import { openapi } from "@elysiajs/openapi"
+
+const app = new Elysia()
+
+
+// Global Logger
+app.onRequest(({ request }) => {
+  console.log("📥", request.method, request.url)
+  console.log("🕒", new Date().toISOString())
+})
+
+
+app.onRequest(({ request, set }) => {
+  if (request.headers.get("x-block") === "true") {
+    set.status = 403
+    return { message: "Blocked" }
+  }
+})
+
+app.get("/", () => "Hello Middleware")
+
+app.get(
+ "/dashboard",
+ () => ({
+   message: "Welcome to Dashboard"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (!headers.authorization) {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+   }
+ }
+)
+
+app.get(
+ "/admin",
+ () => ({
+   message: "Welcome to Admin"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (headers.authorization !== "Bearer 123") {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+     else if (headers.authorization === "Bearer 123") {
+       set.status = 99
+       return {
+         success: true,
+         message: "Authorized"
+       }
+     }
+   }
+ }
+)
+
+app.onAfterHandle(({ response }) => {
+ return {
+   success: true,
+   data: response
+ }
+})
+
+
+app.get("/profile", () => ({
+ name: "Nama kamu"
+}))
+
+app.post(
+ "/register",
+ ({ body }) => body,
+ {
+   body: t.Object({
+     name: t.String({ minLength: 3 }),
+     email: t.String({ format: "email" })
+   })
+ }
+)
+
+
+app.onError(({ code, error, set }) => {
+
+
+ if (code === "VALIDATION") {
+   set.status = 400
+   return {
+     success: false,
+     message: "Validation Error",
+     detail: error.message
+   }
+ }
+
+
+ if (code === "NOT_FOUND") {
+   set.status = 404
+   return {
+     message: "Route not found"
+   }
+ }
+
+
+ set.status = 500
+ return {
+   message: "Internal Server Error"
+ }
+})
+
+
+
+app.listen(3000)
+console.log("Server running at http://localhost:3000")
